0b4757e0d05c3e7f8c119b356ace260c0a81f7fa
lexy <lexyblazy@gmail.com>
2018-08-25 08:58:46 +0100

    adding commentary notes

--- Algorithms/cipher/index.js
+++ Algorithms/cipher/index.js
@@ -1,3 +1,13 @@
+/** 
+  A cipher given a salt, encrypts a string by shifiting 
+  it up/down the alphabets. Example
+  cipher('abcd',2) => 'cdef'
+  cipher('cdef,-2) => 'abcd'
+  cipher("I love JavaScript!", 100) => "E hkra FwrwOynelp!";
+  cipher('E hkra FwrwOynelp!', -100) => "I love JavaScript!";
+
+*/
+
 const cipher = (str, num) => {
   const alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
   let input = str.toLowerCase();
