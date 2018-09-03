8a24c40fc5ee5e03220ca196f34cb072dcff982f
lexy <lexyblazy@gmail.com>
2018-09-03 10:33:47 +0100

    deleted algorithm solution

--- Algorithms/reverse_words/index.js
+++
@@ -1,9 +0,0 @@
-const { reverse } = require("../reverse_string/index");
-
-const reverseWords = words => {
-  return words
-    .split(" ")
-    .map(w => reverse(w))
-    .join(" ");
-};
-module.exports = reverseWords;
